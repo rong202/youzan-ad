a66e2ad31664d66935efdd4da47b05791e6f2e6c
liqiang <2415621370@qq.com>
2019-03-26 15:58:34 +0800

    广告投放系统得实体类

---
+++ youzan-ad-service/youzan-ad-sponsor/src/main/java/com/youzan/ad/SponsorApplication.java
@@ -0,0 +1,18 @@
+package com.youzan.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created by baimugudu on 2019/3/26
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SponsorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SponsorApplication.class,args);
+    }
+}
