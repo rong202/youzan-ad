dc084d41d4ee83bbd2db1bab85499c5a62df3097
liqiang <2415621370@qq.com>
2019-04-01 14:08:04 +0800

    新建广告检索系统，服务调用,服务容错（断路器）

--- youzan-ad-service/youzan-ad-search/src/main/java/com/youzan/ad/client/SponeorClient.java
+++ youzan-ad-service/youzan-ad-search/src/main/java/com/youzan/ad/client/SponeorClient.java
@@ -13,14 +13,17 @@ import java.util.List;
 /**
  * Created by baimugudu on 2019/4/1
  */
-
-@FeignClient(value = "AD-SPONSOR")
+//,fallback=SponeorClientHystrix.class
+@FeignClient(value = "AD-SPONSOR",fallback=SponeorClientHystrix.class)
 public interface SponeorClient {
 
 
     @RequestMapping(value = "/ad-sponsor/plan/getPlan",method = RequestMethod.POST)
     CommonResponse<List<AdPlanResponse>> getPlan(
             @RequestBody AdPlanGetRequest request);
+
+
+
 
 
 }
