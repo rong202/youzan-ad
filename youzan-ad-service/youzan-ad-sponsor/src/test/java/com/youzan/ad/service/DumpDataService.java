f8e01fccde95a0cd40687b61f35d44197f120f14
liqiang <2415621370@qq.com>
2019-04-03 16:02:04 +0800

    索引实现的缓存

--- youzan-ad-service/youzan-ad-sponsor/src/test/java/com/youzan/ad/service/DumpDataService.java
+++ youzan-ad-service/youzan-ad-sponsor/src/test/java/com/youzan/ad/service/DumpDataService.java
@@ -10,13 +10,17 @@ import com.youzan.ad.dao.unit_condition.AdCreativeUnitRepository;
 import com.youzan.ad.dao.unit_condition.AdUnitDistrictRepository;
 import com.youzan.ad.dao.unit_condition.AdUnitItRepository;
 import com.youzan.ad.dao.unit_condition.AdUnitKeywordRepository;
-import com.youzan.ad.dump.table.AdCreativeTbale;
-import com.youzan.ad.dump.table.AdPlanTable;
-import com.youzan.ad.dump.table.AdUnitTable;
+import com.youzan.ad.dump.DConstant;
+import com.youzan.ad.dump.table.*;
 import com.youzan.ad.entity.AdCreative;
 import com.youzan.ad.entity.AdPlan;
 import com.youzan.ad.entity.AdUnit;
+import com.youzan.ad.entity.unit_condition.AdCreativeUnit;
+import com.youzan.ad.entity.unit_condition.AdUnitDistrict;
+import com.youzan.ad.entity.unit_condition.AdUnitIt;
+import com.youzan.ad.entity.unit_condition.AdUnitKeyword;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -55,6 +59,37 @@ public class DumpDataService {
     private AdUnitDistrictRepository adUnitDistrictRepository;
     @Autowired
     private AdUnitKeywordRepository adUnitKeywordRepository;
+
+
+
+
+    @Test
+    public void dumpAdTableData(){
+        dumpAdPlanTable(String.format("%s%s", DConstant.DATA_ROOT_DIR,
+                DConstant.AD_PLAN));
+
+        dumpUnitTable(String.format("%s%s",DConstant.DATA_ROOT_DIR,
+                DConstant.AD_UNIT));
+
+        dumpCreativeTable(String.format("%s%s",DConstant.DATA_ROOT_DIR,
+                DConstant.AD_CREATIVE));
+
+        dumpCreativeUnitTable(String.format("%s%s",DConstant.DATA_ROOT_DIR,
+                DConstant.AD_CREATIVE_UNIT));
+
+        dumpCreativeDistrictTable(String.format("%s%s",DConstant.DATA_ROOT_DIR,
+                DConstant.AD_UNIT_DISTRICT));
+
+        dumpCreativeItTable(String.format("%s%s",DConstant.DATA_ROOT_DIR,
+                DConstant.AD_UNIT_IT));
+
+        dumpCreativeKeyWord(String.format("%s%s",DConstant.DATA_ROOT_DIR,
+                DConstant.AD_UNIT_KEYWORD));
+
+
+
+    }
+
 
 
     private void dumpAdPlanTable(String fileName){
@@ -163,6 +198,143 @@ public class DumpDataService {
 
 
     }
+
+
+    private void dumpCreativeUnitTable(String fileName){
+
+        List<AdCreativeUnit> adCreativeUnits = adCreativeUnitRepository.findAll();
+        List<AdCreativeUnitTbale> adCreativeUnitTbales = new ArrayList<>();
+        adCreativeUnits.forEach(
+                ad->adCreativeUnitTbales.add(
+                        new AdCreativeUnitTbale(
+                                ad.getUnitId(),
+                                ad.getCreativeId()
+                        )
+                )
+        );
+
+
+       Path path =  Paths.get(fileName);
+
+       try(BufferedWriter writer = Files.newBufferedWriter(path)){
+           for(AdCreativeUnitTbale adCreativeUnitTbale:adCreativeUnitTbales){
+               writer.write(JSON.toJSONString(adCreativeUnitTbale));
+               writer.newLine();
+           }
+
+           writer.close();
+
+
+       }catch(IOException e){
+           e.printStackTrace();
+        }
+
+
+    }
+
+
+    private void dumpCreativeDistrictTable(String fileName){
+
+         List<AdUnitDistrict> adUnitDistricts = adUnitDistrictRepository.findAll();
+         List<AdUnitDistrictTable> adUnitDistrictTables = new ArrayList<>();
+
+        adUnitDistricts.forEach(
+                i->adUnitDistrictTables.add(
+                        new AdUnitDistrictTable(
+                                i.getUnitId(),
+                                i.getProvince(),
+                                i.getCity()
+
+                        )
+                )
+        );
+
+        Path path = Paths.get(fileName);
+        try(BufferedWriter writer = Files.newBufferedWriter(path)){
+            for(AdUnitDistrictTable adUnitDistrictTable:adUnitDistrictTables){
+                writer.write(JSON.toJSONString(adUnitDistrictTable));
+                writer.newLine();
+            }
+            writer.close();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+
+
+
+    private void dumpCreativeItTable(String fileName){
+
+        List<AdUnitIt> adUnitIts = adUnitItRepository.findAll();
+        List<AdUnitItTable> adUnitItTables = new ArrayList<>();
+
+        adUnitIts.forEach(
+                i->adUnitItTables.add(
+                        new AdUnitItTable(
+                                i.getUnitId(),
+                                i.getItTag()
+                        )
+
+                        )
+
+        );
+
+        Path path = Paths.get(fileName);
+        try(BufferedWriter writer = Files.newBufferedWriter(path)){
+            for(AdUnitItTable adUnitItTable:adUnitItTables){
+                writer.write(JSON.toJSONString(adUnitItTable));
+                writer.newLine();
+            }
+            writer.close();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+
+
+    private void dumpCreativeKeyWord(String fileName){
+
+        List<AdUnitKeyword> adUnitKeywords = adUnitKeywordRepository.findAll();
+        List<AdUnitKeywordTable> adUnitItTables = new ArrayList<>();
+
+        adUnitKeywords.forEach(
+                i->adUnitItTables.add(
+                        new AdUnitKeywordTable(
+                               i.getUnitId(),
+                                i.getKeyword()
+                        )
+
+                )
+
+        );
+
+        Path path = Paths.get(fileName);
+        try(BufferedWriter writer = Files.newBufferedWriter(path)){
+            for(AdUnitKeywordTable adUnitItTable:adUnitItTables){
+                writer.write(JSON.toJSONString(adUnitItTable));
+                writer.newLine();
+            }
+            writer.close();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+
 
 
 
