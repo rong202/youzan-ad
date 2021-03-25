2f646a1e44fa326fe596542c1170a80edbb56686
Rong <feis202@163.com>
2021-03-25 09:54:18 +0800

    调整格式

--- youzan-ad-service/youzan-ad-search/src/main/java/com/youzan/ad/index/adunit/AdUnitConstants.java
+++ youzan-ad-service/youzan-ad-search/src/main/java/com/youzan/ad/index/adunit/AdUnitConstants.java
@@ -5,7 +5,7 @@ package com.youzan.ad.index.adunit;
  */
 public class AdUnitConstants {
 
-    public static class POSITION_TYPE{
+        public static class POSITION_TYPE{
         public static final int KAIPING = 1;
         public static final int TIEPIAN = 2;
         public static final int TIEPIAN_MIDDLE = 4;
